d188a5d6031dbe4579eb1b1b29deecc55416dfc3
jingzhou <jingzhou_wang@hotmail.com>
2024-11-26 01:39:05 +0800

    do.sh: boost 1.86.0, NDK28

--- do.sh
+++ do.sh
@@ -8,17 +8,17 @@
 #----------------------------------------------------------------
 
 # Specify the path to boost source code dir 
-export BOOST_DIR=$(pwd)/down/boost/1.83.0
+export BOOST_DIR=../1.86.0
 
 
 # Where the Android Ndk you want to build with is located
-export NDK_DIR=$(pwd)/down/ndk/26c
+export NDK_DIR=${ANDROID_NDK_ROOT}
 
 # Which target abis (~ architecture + instruction set) you want to build for (separate by spaces)    
 export ABI_NAMES="arm64-v8a armeabi-v7a x86 x86_64"
 
 # Whether to build boost as dynamic or shared libraries (or both)
-export LINKAGES="shared static" # can be "shared" or "static" or "shared static" (both)
+export LINKAGES="static" # can be "shared" or "static" or "shared static" (both)
 
 
 #----------------------------------------------------------------
